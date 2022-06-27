add4025d5471f0b6f51cde6feb774b1ecdcb1ac4
Alba Serrano <x17albaserrano17x@gmail.com>
2022-06-27 20:59:28 +0200

    (eliminar archivos fuera de sitio)

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -11,31 +11,29 @@ class SecurityFilters {
                 def accionesDefault = ['modulo':['show', 'index', 'create', 'save'], 'spot':['show','index','create','save','edit','delete', 'update'], 'usuario':['edit', 'logout', 'show', 'update', 'validate'], 'valoracion':['create', 'index', 'show','save']]
 
                 if(!session.user && !accionesAnonimo[controllerName].contains(actionName)){
-                    print("DENEGADO")
                     redirect(controller:'usuario', action:'login', params:['cName':controllerName, 'aName':actionName])
                     return false
                 } else if (session.user && !session.user.admin && !accionesDefault[controllerName].contains(actionName)){
-                    print("acceso denegado USUARIO para " +  actionName + " en " + controllerName)
                     redirect(uri:'/')
                     return false
                 } else if (session.user && !session.user.admin && controllerName == 'usuario' && actionName == 'edit' && params.id != String.valueOf(session.user.id)){
-                    print("ACCESO")
-                    redirect(controller:'usuario', action:actionName, id:session.user.id)
+                    redirect(controller:'usuario', action:'show', id:params.id)
                     return false
                 } else if(session.user && !session.user.admin && controllerName == 'spot' && (actionName == 'edit' || actionName == 'delete')){
                     Spot spotInstance = Spot.findById(params.id)
 
                     if(spotInstance.publicador.id != session.user.id){
+                        redirect(controller:'spot', action:'show', id:params.id)
                         return false
                     }
                 } else if(session.user && !session.user.admin && controllerName == 'modulo' && (actionName == 'edit' || actionName == 'delete')){
                     Modulo moduloInstance = Modulo.findById(params.id)
 
                     if(moduloInstance.publicador.id != session.user.id){
+                        redirect(controller:'usuario', action:actionName, id:session.user.id)
                         return false
                     }
                 }else if (session.user && session.user.admin && controllerName == 'signup' && actionName == 'create'){
-                    print("acceso denegado USUARIO-V2")
                     redirect(uri:'/')
                     return false
                 }
